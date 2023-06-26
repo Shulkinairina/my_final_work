f85646d2ac3bed6780573cbbf4d825dd49fcf185
Shulkinairina <119783249+Shulkinairina@users.noreply.github.com>
2023-06-27 00:24:07 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+comunity_token = 'vk1.a.GaB-1d9QZ4exif0IGROddawvGOABOY4MqIWP7xnPgK2Z9-85tlmR8nw1JuDodDy7vB3pDGJlXN0WOcAzGb0uIpnsS1Lq0ZiomXSikYB18' \
+                 'YajABuNEeBB3ktBH98MQVTzCh6oyVC4h1m3UILPPGstE_vjU-qiB5xfeYWgV9Bxzo65P4BE4FgZybfJXatnT6Fezyl9zB1SXyXPowoHKWgQbA '
+access_token = 'vk1.a.I2PhDXILyKRW0sqvRy3TIPnB9uUKWIlG6n_3bjvxh6l-spUomwHwUvjG0P1HRflo0cH-BPUu4jlZHOLzkpIR8VRk5Ttc_lZr6nokthqrL1YggbiLQ5IByV2l3uhlA-0FWXTuI-'
+db_url_object = 'postgresql+psycopg2://user_name:password@localhost:5432/my_final_work'
